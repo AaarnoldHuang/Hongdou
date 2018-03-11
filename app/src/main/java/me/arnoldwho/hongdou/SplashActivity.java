71cadd00e4e909f8573ecc58bfc117cf4596c5e6
Arnoldwho <arnoldhuang0725@gmail.com>
2018-03-11 23:56:18 +0800

    Add splash

---
+++ app/src/main/java/me/arnoldwho/hongdou/SplashActivity.java
@@ -0,0 +1,26 @@
+package me.arnoldwho.hongdou;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        SystemClock.sleep(1500);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
